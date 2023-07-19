5e77fac54dfd86118bda1fd35b57b78671b53ebf
ts-x4 <47035562+ts-x4@users.noreply.github.com>
2023-07-19 12:48:25 -0400

    Add files via upload

---
+++ Launch Scripts/jzintv-vUHIGH3pr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to launch Intellivision games in jzIntv with bespoke keyboard hackfiles. The hackfile must named the same as rom, including extension, with .kbd added.
+params="$1"
+/opt/retropie/emulators/jzintv/bin/jzintv -p /home/pi/RetroPie/BIOS --gfx-palette=/home/pi/RetroPie/roms/intellivision/gfx-default.gfx --js0="ac,push=43,rels=38" --js1="ac,push=43,rels=38" --kbdhackfile="$params".kbd "$params" -z4 -f1 -q -v1 -s0
---
+++ Launch Scripts/jzintv8dirUHIGH2pr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to launch Intellivision games in jzIntv with bespoke keyboard hackfiles. The hackfile must named the same as rom, including extension, with .kbd added.
+params="$1"
+/opt/retropie/emulators/jzintv/bin/jzintv -p /home/pi/RetroPie/BIOS --gfx-palette=/home/pi/RetroPie/roms/intellivision/gfx-default.gfx --js0="ac,push=33,rels=28" --js1="ac,push=33,rels=28" --kbdhackfile="$params".kbd "$params" -z4 -f1 -q -v0 -s0
